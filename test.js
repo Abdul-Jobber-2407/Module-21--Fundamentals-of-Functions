6623e52f7eb24b52ffc6e5f8f9e3f7b0239c082a
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-21 12:50:32 +0600

    function

---
+++ test.js
@@ -0,0 +1,7 @@
+const price = 45;
+// function declare
+function fanofkor(){
+     console.log('bosa thaka utta dara');
+     console.log('walk towards the switch');
+     console.log('click the switch to oiff the fan')
+}
